b3fa69397c7b48b22342e4f3bb822d627446fa32
root <romain.allain@gmail.com>
2016-04-14 16:46:20 +0200

    add options, change option parsing system

--- read_all_holding_registries.py
+++ read_all_holding_registries.py
@@ -3,13 +3,13 @@
 """
 File: read_all_holding_registries.py
 Desc: Read all holding registries from a TCP MODBUS Slave
-Version: 0.0.1
+Version: 0.0.2
 """
 
 __author__ = 'rm'
 
-from optparse import OptionParser
 from pymodbus.client.sync import ModbusTcpClient
+import argparse
 import sys
 import collections
 
@@ -40,19 +40,19 @@ def validate_ipv4(s):
     try: return all(0<=int(p)<256 for p in pieces)
     except ValueError: return False
 
-def scan(argv):
+def scan():
     
-    # TODO add start/stop addr mechanism
-    parser = OptionParser(
-        usage = "usage: %prog [options] [ip address]",
-        description = """Read all holding registries from a TCP MODBUS Slave""")
-    parser.add_option("--port", dest="port", help="Modbus port", type="int", metavar="PORT", default="502")
-    parser.add_option("--uid", dest="uid", help="Modbus Unit ID", type="int", metavar="UID", default="1")
+    parser = argparse.ArgumentParser(description = "Read all holding registries from a TCP MODBUS Slave")
+    parser.add_argument("ip", help="IP address of the slave")
+    parser.add_argument("-p", "--port", dest="port", help="Modbus Port. Defaults to 502", type=int, metavar="PORT", default=502)
+    parser.add_argument("-u", "--uid", dest="uid", help="Modbus Unit ID. Defaults to 1", type=int, metavar="UID", default=1)
+    parser.add_argument("-sa", "--start-address", dest="start_address", help="Starting Address for the scanner. Defaults to 1", type=int, metavar="START", default=1)
+    parser.add_argument("-ea", "--end-address", dest="end_address", help="Ending Address for the scanner. Defaults to 65535", type=int, metavar="END", default=65535)
     
-    (options, args) = parser.parse_args(argv)
+    args = parser.parse_args()
     
     try:
-        ip = args[0]
+        ip = args.ip
     except IndexError:
         print "ERROR: No target to scan\n\n"
         parser.print_help()
@@ -66,7 +66,7 @@ def scan(argv):
 
     print 'Connecting to %s...' % ip,
     # connect to modbus slave
-    client = ModbusTcpClient(ip, options.port)
+    client = ModbusTcpClient(ip, args.port)
     client.connect()
     if client.socket == None:
         print "ERROR: Could not connect to %s." %ip
@@ -76,14 +76,14 @@ def scan(argv):
     # TODO add ETA mechanism
     results = {}
     addr = 1
-    for addr in range(1, 65535):
-        hr = client.read_holding_registers(addr, 1, unit=options.uid) # unit value is device id of the slave (UID)
+    for addr in range(args.start_address, args.end_address):
+        hr = client.read_holding_registers(addr, 1, unit=args.uid) # unit value is device id of the slave (UID)
         if hr.function_code == 3: # if we succeed reading stuff
             results[addr] = hr.registers[0]
         # if it fails, hr.function = 131 (0x83), cf modbus doc
 
     client.close()
-    print 'Register scanning is finished (65535 registers were tried)' # TODO add stop-start+1
+    print 'Register scanning is finished (%d registers were tried)' % (args.end_address-args.start_address+1)
     # sorting dict for printing
     ordered_results = collections.OrderedDict(sorted(results.items()))
     for addr, value in ordered_results.iteritems():
@@ -91,6 +91,6 @@ def scan(argv):
 
 if __name__=="__main__":
     try:
-        scan(sys.argv[1:])
+        scan()
     except KeyboardInterrupt:
         status("Ctrl-C happened\n")
